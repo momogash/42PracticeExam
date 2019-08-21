686928ace144e54a57318eb7cb3425244d5a1b8f
Moloko MOGASHOA <momogash@c5r7s3.wethinkcode.co.za>
2019-08-21 13:15:06 +0200

    print_hex

--- level03/print_hex.c
+++ level03/print_hex.c
@@ -1,47 +1,45 @@
 #include <unistd.h>
 
-int ft_atoi(char *str)
+int	ft_atoi(char *str)
 {
-        int sign;
-        int i;
-        int result;
+	int sign;
+	int result;
+	int i;
 
-        i = 0;
-        sign = 1;
-        if  (str[0] == '-')
-        {
-                sign = -1;
-                i++;
-        }
-        result = 0;
-        while   (str[i] == '\t'|| str[i] == '\v'|| str[i] == '\n' || str[i] == ' '|| str[i] == '\r' || str[i] == '\f')
-                i++;
-        while   (str[i] && (str[i] >= '0' && '9'>= str[i]))
-        {
-                result = result * 10 + str[i] - '0';
-                i++;
-        }
-        return(result * sign);
+	i = 0;
+	sign = 1;
+	if	(str[0] == '-')
+	{
+		sign = -1;
+		i++;
+	}
+	result = 0;
+	while	(str[i] == '\t' || str[i] == '\f' || str[i] == ' ' || str[i] == '\n' || str[i] == '\r')
+		i++;
+	while	(str[i] != '\0' && (str[i] >= '0' && '9'>= str[i]))
+	{
+		result = result * 10 + str[i] - '0';
+		i++;
+	}
+	return(result * sign);
+
 }
 
 void print_hex(int nbr)
 {
-        char *str;
-
-        str = "0123456789abcdef";
-        if  (nbr == '0')
-                write(1, "0", 1);
-        while   (nbr)
-        {
-                write(1, &str[nbr % 16], 1);
-                nbr = nbr / 16;
-        }
+	if	(nbr >= 16)
+		print_hex(nbr / 16);
+	if	(nbr % 16 < 10)
+		nbr = (nbr % 16) + '0';
+	else
+		nbr = (nbr % 16) - 10 + 'a';
+	write(1, &nbr, 1);
 }
 
 int main(int ac, char *av[])
 {
-        if  (ac == 2)
-                print_hex(ft_atoi(av[1]));
-        write(1, "\n", 1);
-        return(0);
+	if	(ac == 2)
+		print_hex(ft_atoi(av[1]));
+	write(1, "\n", 1);
+	return(0);
 }
