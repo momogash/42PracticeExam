4d6e728a81f4e6e9c2f9abcda4d0f4cdc8f3d9a9
Moloko MOGASHOA <momogash@c5r7s3.wethinkcode.co.za>
2019-08-28 16:56:30 +0200

    rostring

---
+++ level04/rostring.c
@@ -0,0 +1,62 @@
+#include <unistd.h>
+#include <stdio.h>
+int space(char c)
+{
+	if  (c == ' ' || c == '\t')
+		return(1);
+	return(0);
+}
+
+void rotstring(char *str)
+{
+	int i;
+	int startFirstWord;
+	int endFirstWord;
+	int len;
+
+	i = 0;
+	startFirstWord = 0;
+	endFirstWord = 0;
+	len = 0;
+
+	while  (space(str[i]) && str[i])
+		i++;
+	startFirstWord = i;
+	printf("firstword starts at %d\n", i); 
+	while  (!space(str[i]) && str[i])
+		i++;
+	endFirstWord = i;
+	printf("first word ends at %d\n", i);
+	while   (str[i] != '\0' && space(str[i]))
+		i++; 
+	while	(str[i] != '\0')
+	{
+		if	(space(str[i]) && str[i])
+			i++;
+		else 
+		{	
+			while (!space(str[i]) && str[i])
+			{
+				write(1, &str[i], 1);
+				i++;
+			}
+		   write(1, " ", 1);	
+		}
+	}
+	if	(str[i] == '\0')
+	{
+		while (startFirstWord < endFirstWord)
+		{
+			write(1, &str[startFirstWord], 1);
+			startFirstWord++;
+		}
+	}
+}
+
+int main(int ac, char *av[])
+{
+	if  (ac > 1)
+		rotstring(av[1]);
+	write(1, "\n", 1);
+	return(0);
+}
