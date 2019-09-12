1dd82007a388782ebed61f531cb37ba2e87ec521
Moloko MOGASHOA <momogash@c5r7s3.wethinkcode.co.za>
2019-09-12 17:58:23 +0200

    split rev_w

--- level04/rev_wstr2.c
+++ level04/rev_wstr2.c
@@ -1,21 +1,21 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   rostring2.c                                        :+:      :+:    :+:   */
+/*   rev_wstr2.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: momogash <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/09/12 13:35:17 by momogash          #+#    #+#             */
-/*   Updated: 2019/09/12 15:58:20 by momogash         ###   ########.fr       */
+/*   Created: 2019/09/12 17:16:18 by momogash          #+#    #+#             */
+/*   Updated: 2019/09/12 17:57:09 by momogash         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
 #include <unistd.h>
-#include <stdlib.h>
 #include <string.h>
+#include <stdlib.h>
 
 char    **ft_split(char *str)
 {
+
 	char **arr;
 	int i;
 	int j;
@@ -24,32 +24,28 @@ char    **ft_split(char *str)
 	i = 0;
 	j = 0;
 
-	if(!*str)
+	if	(!str[i])
 		return(NULL);
 
 	arr = (char **)malloc(sizeof(char *) * 1024);
 	if	(arr == NULL)
 		return(NULL);
-
 	while	(str[i] != '\0')
 	{
 		arr[j] = (char *)malloc(sizeof(char) * 1024);
 		if	(arr == NULL)
 			return(NULL);
-
-		while	(( str[i] == ' ' || str[i] == '\t') && str[i] != '\0')
-			i++;
-
 		if	(str[i] == '\0')
 			break;
+		while	((str[i] == ' ' || str[i] == '\t') && str[i] != '\0')
+			i++;
 		k = 0;
-
 		while	(!(str[i] == ' ' || str[i] == '\t') && str[i] != '\0')
 		{
 			arr[j][k] = str[i];
 			k++;
 			i++;
-		}	
+		}
 		arr[j][k] = '\0';
 		j++;
 	}
@@ -69,26 +65,26 @@ void	ft_putstr(char *str)
 	}
 }
 
-
 int main(int ac, char *av[])
 {
-	char **res;
+	char **arr;
 	int i;
-
-	i = 1;
-
+	i = 0;
 	if	(ac == 2)
 	{
-		res = ft_split(av[1]);
-		while	(res[i])
-		{
-			ft_putstr(res[i]);
-			ft_putstr(" ");
+
+		arr = ft_split(av[1]);
+		while	(arr[i] != NULL)
 			i++;
+		i--;
+		while	(i >= 0)
+		{
+			ft_putstr(arr[i]);
+			i--;
+			if	(i >= 0)
+				ft_putstr(" ");
 		}
-		ft_putstr(res[0]);
 	}
 	ft_putstr("\n");
 	return(0);
 }
-
